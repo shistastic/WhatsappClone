7d9418a0d5481801726e6e475e06887ecf03a05c
shistastic <adamjamesrowe@outlook.es>
2021-06-24 23:02:45 +0200

    User Card Complete

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -119,6 +119,103 @@ class _HomeState extends State<Home> {
                 ],
               ),
             ),
+            Container(
+              width: double.infinity,
+              height: double.maxFinite,
+              child: ListView(
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(16, 13, 10, 10),
+                          child: Container(
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(vertical: 10),
+                            ),
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    'assets/images/saida-whatsapp.png',
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                                shape: BoxShape.circle,
+                                color: Color(0xff7fb0a9)),
+                          ),
+                        ),
+                        Container(
+                          height: 85,
+                          width: MediaQuery.of(context).size.width - 100,
+                          decoration: BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                                    color: Color(0xfff1f1f1), width: 2)),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(
+                                      10,
+                                      20,
+                                      0,
+                                      5,
+                                    ),
+                                    child: Text(
+                                      'Saida',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.black),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(
+                                      10,
+                                      0,
+                                      0,
+                                      4,
+                                    ),
+                                    child: Text(
+                                      'JAJJAJAJAJA',
+                                      style: TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.normal,
+                                          color: Colors.grey[500]),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(
+                                  10,
+                                  0,
+                                  0,
+                                  20,
+                                ),
+                                child: Text(
+                                  '23:15',
+                                  style: TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.normal,
+                                      color: Colors.grey[500]),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
